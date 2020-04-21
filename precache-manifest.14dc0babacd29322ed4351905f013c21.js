71d06d5b53a045343c2806f6584591e9f45d9c77
jlozovei <jlozovei@users.noreply.github.com>
2020-04-21 17:54:48 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.14dc0babacd29322ed4351905f013c21.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89fb6f26e5630bc42b46fe56cc46c4ce",
+    "url": "./index.html"
+  },
+  {
+    "revision": "96f750db207915388020",
+    "url": "./static/css/main.3377b14e.chunk.css"
+  },
+  {
+    "revision": "fdd47fdbea3023cd8d49",
+    "url": "./static/js/2.dd1064c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.dd1064c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96f750db207915388020",
+    "url": "./static/js/main.c4e9c905.chunk.js"
+  },
+  {
+    "revision": "301637df0067f666e52d",
+    "url": "./static/js/runtime-main.31f47091.js"
+  },
+  {
+    "revision": "bb0872dfe4e88e5e31e0e5f2b04ae124",
+    "url": "./static/media/galaxy.bb0872df.png"
+  },
+  {
+    "revision": "051cdd951cf8786d57fb1e5bd3799e53",
+    "url": "./static/media/news-gothic-webfont.051cdd95.eot"
+  },
+  {
+    "revision": "10a9e3f69acd03ae9c4d407636805778",
+    "url": "./static/media/news-gothic-webfont.10a9e3f6.ttf"
+  },
+  {
+    "revision": "85bbda7e94563f4301dadc6c183e8490",
+    "url": "./static/media/news-gothic-webfont.85bbda7e.woff"
+  },
+  {
+    "revision": "e6aa3e78779ce512821e19805ca19a8b",
+    "url": "./static/media/news-gothic-webfont.e6aa3e78.svg"
+  },
+  {
+    "revision": "e92d0cbe5316c8e86094ac851ef1c0ac",
+    "url": "./static/media/star-wars-opening-theme.e92d0cbe.mp3"
+  },
+  {
+    "revision": "1a4fd78592d6aabe0247e018ffe94b9d",
+    "url": "./static/media/universal-webfont.1a4fd785.svg"
+  },
+  {
+    "revision": "64004c8c1b994be624f25060227a4b98",
+    "url": "./static/media/universal-webfont.64004c8c.woff"
+  },
+  {
+    "revision": "98ce01e59bd6367988e1d5ec9d5cc89a",
+    "url": "./static/media/universal-webfont.98ce01e5.woff2"
+  },
+  {
+    "revision": "c32719ca197ef64631cdbf2e59c1855c",
+    "url": "./static/media/universal-webfont.c32719ca.eot"
+  },
+  {
+    "revision": "e04de82595a72b23d5d9e1a099db410b",
+    "url": "./static/media/universal-webfont.e04de825.ttf"
+  }
+]);
